b3116361e22e155561ccd6a145bb085573d0d709
git-kafkanii <git.kafkanii@gmail.com>
2022-10-18 11:24:09 +0300

    Interfaces

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Main.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType extends BaseEntity {
+
+    private String petName;
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+}
